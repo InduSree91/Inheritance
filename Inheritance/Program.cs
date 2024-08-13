f3522dd54d7900ccda607d4947c0a7628da367ec
Indu Sree <Indu Sree@DESKTOP-UN8GH21>
2024-08-13 13:13:23 +0530

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car { Wheels = 4, Color = "Black" ,Brand = "Toyota", Model = "Corolla", Year = 2024 };
+            myCar.Start();
+            myCar.Driving();
+        }
+    }
+
+    class Vehicle
+    {
+        public int Wheels { get; set; }
+        public string Color { get; set; }
+
+        public void Driving()
+        {
+            Console.WriteLine("Driver is driving the Vehicle");
+        }
+    }
+
+    class Car : Vehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void Start()
+        {
+            Console.WriteLine("Car has started");
+        }
+    }
+}
